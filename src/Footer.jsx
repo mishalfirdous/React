278ab10bc552671fd941bfd83d9df606d648add2
mishalfirdous <mishal.firdous@codeninjaconsulting.com>
2024-01-29 12:32:06 +0500

    chnages on both files accordingly

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   const linkedInProflie = 'https://www.linkedin.com/in/mishal-firdous-b99a0a131/';
 
   return (
-    <footer className='FooterSection'>
+    <footer className='footer-section'>
       <p>Connect with me on LinkedIn: <a href={linkedInProflie} target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
     </footer>
   );
